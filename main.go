4ed4666518b99cb4364e2cb0459a38847b1144f2
Vlad Zimmerl <vladzimmerl@gmail.com>
2025-03-11 12:05:32 -0400

    cors

--- main.go
+++ main.go
@@ -135,7 +135,7 @@ func handleError(err error) {
 
 func CORS(next http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Add("Access-Control-Allow-Origin", "*")
+		w.Header().Add("Access-Control-Allow-Origin", "todo-typescript-frontend.pages.dev")
 		//w.Header().Add("Access-Control-Allow-Origin", "https://todo-typescript-frontend.pages.dev")
 		w.Header().Add("Access-Control-Allow-Credentials", "true")
 		w.Header().Add("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
