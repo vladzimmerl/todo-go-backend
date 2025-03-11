34489155ffb22b5a0aa9b75880b0928b22634b62
Vlad Zimmerl <vladzimmerl@gmail.com>
2025-03-11 12:12:53 -0400

    cors

--- main.go
+++ main.go
@@ -135,16 +135,13 @@ func handleError(err error) {
 
 func CORS(next http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Add("Access-Control-Allow-Origin", "https://todo-typescript-frontend.pages.dev")
-		//w.Header().Add("Access-Control-Allow-Origin", "https://todo-typescript-frontend.pages.dev")
+		origin := r.Header.Get("Origin")
+		if origin == "https://todo-typescript-frontend.pages.dev" || origin == "https://todo.vladzimmerl.com" {
+			w.Header().Add("Access-Control-Allow-Origin", origin)
+		}
 		w.Header().Add("Access-Control-Allow-Credentials", "true")
 		w.Header().Add("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
 		w.Header().Add("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
-
-		if r.Method == "OPTIONS" {
-			http.Error(w, "No Content", http.StatusNoContent)
-			return
-		}
 
 		next(w, r)
 	}
